79fd35d7c4ae71fa7fa190e4d1e6370ba6552118
Michael Knott <knottweb@gmail.com>
2021-03-20 12:34:18 +0000

    add other apis

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 10.times do
   newtwilio = Twilio.create(data: "Twilio API Data")
-  newaircall = Aircall.create(data: "Twilio API Data")
-  newasana = Asana.create(data: "Twilio API Data")
-  newkissflow = Kissflow.create(data: "Twilio API Data")
-  newgusto = Gusto.create(data: "Twilio API Data")
-  newgitlab = Gitlab.create(data: "Twilio API Data")
-  newrydoo = Rydoo.create(data: "Twilio API Data")
-  newstripe = Stripe.create(data: "Twilio API Data")
-  newpostman = Postman.create(data: "Twilio API Data")
-  newcasavi = Casavi.create(data: "Twilio API Data")
+  newaircall = Aircall.create(data: "Aircall API Data")
+  newasana = Asana.create(data: "Asana API Data")
+  newkissflow = Kissflow.create(data: "Kissflow API Data")
+  newgusto = Gusto.create(data: "Gusto API Data")
+  newgitlab = Gitlab.create(data: "Gitlab API Data")
+  newrydoo = Rydoo.create(data: "Rydoo API Data")
+  newstripe = Stripe.create(data: "Stripe API Data")
+  newpostman = Postman.create(data: "Postman API Data")
+  newcasavi = Casavi.create(data: "Casavi API Data")
 end
 
 puts "Fake API data created"
