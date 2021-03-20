e967c8bbe77958de13a5ffa65646af713b7ae1ea
Michael Knott <knottweb@gmail.com>
2021-03-20 11:27:01 +0000

    response headers add: sunset and deprecated

--- app/controllers/api/v1/api_ones_controller.rb
+++ app/controllers/api/v1/api_ones_controller.rb
@@ -1,6 +1,10 @@
+require'date'
+
 class Api::V1::ApiOnesController < ApplicationController
   def index
     @api_ones = ApiOne.all
+    response.set_header('Sunset', DateTime.new(2021,3,21,12,0,0))
+    response.set_header('Deprecated', false)
     render json: @api_ones
   end
 end
