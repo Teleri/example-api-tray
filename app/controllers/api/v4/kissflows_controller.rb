11b058943d60d7006f0a3c304ac1251c30751581
Michael Knott <knottweb@gmail.com>
2021-03-23 01:06:35 +0000

    test for hotwire purposes

--- app/controllers/api/v4/kissflows_controller.rb
+++ app/controllers/api/v4/kissflows_controller.rb
@@ -3,6 +3,7 @@ require'date'
 class Api::V4::KissflowsController < ApplicationController
   def index
     @kissflows = Kissflow.all
+    response.set_header('Sunset', DateTime.new(2456,01,31,12,0,0))
     response.set_header('Deprecated', true)
     render json: @kissflows
   end
