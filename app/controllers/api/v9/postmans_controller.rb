fd7d4eeb7382eb7d811a04d6cc0bc6d44168f07b
Michael Knott <knottweb@gmail.com>
2021-03-23 00:57:14 +0000

    test for hotwire purposes

--- app/controllers/api/v9/postmans_controller.rb
+++ app/controllers/api/v9/postmans_controller.rb
@@ -3,7 +3,7 @@ require'date'
 class Api::V9::PostmansController < ApplicationController
   def index
     @postmans = Postman.all
-    # response.set_header('Sunset', nil)
+    response.set_header('Sunset', DateTime.new(2022,01,31,12,0,0))
     response.set_header('Deprecated', false)
     render json: @postmans
   end
