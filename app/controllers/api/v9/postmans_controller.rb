b0b04a2bd9b753b5ea819886b502384d54cde065
Michael Knott <knottweb@gmail.com>
2021-03-23 00:52:31 +0000

    test for hotwire purposes

--- app/controllers/api/v9/postmans_controller.rb
+++ app/controllers/api/v9/postmans_controller.rb
@@ -4,7 +4,7 @@ class Api::V9::PostmansController < ApplicationController
   def index
     @postmans = Postman.all
     # response.set_header('Sunset', nil)
-    response.set_header('Deprecated', true)
+    response.set_header('Deprecated', false)
     render json: @postmans
   end
 end
